2548bf7618796332fd99c6988124b8d09f8179c5
rteddylee <rteddylee62@hotmail.com>
2017-05-21 11:47:05 -0400

    Initial commit of code base

---
+++ src/EnvironmentSensor/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 5/14/2017 1:00:16 PM
+ * Author : jadams
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
